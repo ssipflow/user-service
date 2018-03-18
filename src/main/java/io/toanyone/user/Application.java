3fd8803575f27ad62b893f3de0e17e2ab0a8e183
leonardo.park <blue.park@kt.com>
2018-03-18 23:30:38 +0900

    add ingress

--- src/main/java/io/toanyone/user/Application.java
+++ src/main/java/io/toanyone/user/Application.java
@@ -1,9 +1,14 @@
 package io.toanyone.user;
 
+import io.toanyone.user.domain.User;
+import io.toanyone.user.repository.UserRepository;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @EnableCircuitBreaker
@@ -12,5 +17,28 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	private UserRepository repository;
+
+
+	@Bean
+	InitializingBean sendDatabase() {
+		User user = new User();
+
+		user.setSeq(1);
+		user.setRealName("Leonardo Park");
+		user.setUserName("user");
+		user.setUserPass("12345678");
+		user.setEnabled(true);
+		user.setCredentialsNonExpired(true);
+		user.setAccountNonExpired(true);
+		user.setAccountNonLocked(true);
+		user.setRoles("ROLE_USER");
+
+		return () -> {
+			repository.save(user);
+		};
 	}
 }
