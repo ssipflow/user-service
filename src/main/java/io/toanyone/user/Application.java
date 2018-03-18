bb966c7547ad03bea9e9150f8023528433055295
leonardo.park <blue.park@kt.com>
2018-03-18 22:45:15 +0900

    add hystrix

--- src/main/java/io/toanyone/user/Application.java
+++ src/main/java/io/toanyone/user/Application.java
@@ -2,7 +2,11 @@ package io.toanyone.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class Application {
 
