769779d4f6ca19a4c974360b456a0f5260a3799b
leonardo.park <blue.park@kt.com>
2018-03-20 00:18:57 +0900

    add hystrix properties (it is beta...)

--- src/main/java/io/toanyone/user/Application.java
+++ src/main/java/io/toanyone/user/Application.java
@@ -2,18 +2,12 @@ package io.toanyone.user;
 
 import io.toanyone.user.domain.User;
 import io.toanyone.user.repository.UserRepository;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.context.annotation.Bean;
 
-@EnableEurekaClient
-@EnableCircuitBreaker
-@SpringBootApplication
+@SpringCloudApplication
 public class Application {
 
 	public static void main(String[] args) {
